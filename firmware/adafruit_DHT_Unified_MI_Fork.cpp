dc0c4498e5ce18ac99541118c3eb7565a322fd6b
reallylongaddress <dominic@mobileigniter.com>
2015-05-10 14:48:33 -0500

    hacking

--- firmware/adafruit_DHT_Unified_MI_Fork.cpp
+++ firmware/adafruit_DHT_Unified_MI_Fork.cpp
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-#include "DHT_U.h"
+#include "adafruit_DHT_Unified_MI_Fork.h"
 
 DHT_Unified::DHT_Unified(uint8_t pin, uint8_t type, uint8_t count, int32_t tempSensorId, int32_t humiditySensorId):
   _dht(pin, type, count),
--- firmware/adafruit_DHT_Unified_MI_Fork.h
+++ firmware/adafruit_DHT_Unified_MI_Fork.h
@@ -22,8 +22,10 @@
 #ifndef DHT_U_H
 #define DHT_U_H
 
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
+//#include <Adafruit_Sensor.h>
+#include "Adafruit_Sensor/Adafruit_Sensor.h"
+//#include <DHT.h>
+#include "Adafruit_DHT/Adafruit_DHT.h"
 
 #define DHT_SENSOR_VERSION 1
 
